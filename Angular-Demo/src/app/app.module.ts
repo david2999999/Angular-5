642e1f12d25a947ca43725b2f54f0ff5b08150f2
David Jiang <djiang86@binghamton.edu>
2018-04-23 16:00:42 +0000

    string interpolation, 2 way data binding, event binding, and property binding

--- Angular-Demo/src/app/app.module.ts
+++ Angular-Demo/src/app/app.module.ts
@@ -9,6 +9,7 @@ import {ServerComponent} from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { UsernameComponent } from './username/username.component';
 
 
 @NgModule({
@@ -17,8 +18,10 @@ import { SuccessAlertComponent } from './success-alert/success-alert.component';
     ServerComponent,
     ServersComponent,
     WarningAlertComponent,
-    SuccessAlertComponent
+    SuccessAlertComponent,
+    UsernameComponent
   ],
+
   imports: [
     BrowserModule,
     FormsModule,
