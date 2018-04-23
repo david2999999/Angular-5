a94f66374f4b840f2dcf715a1bd695628071491c
David Jiang <djiang86@binghamton.edu>
2018-04-23 13:14:43 +0000

    creating another component and link it inside app.modules

--- Angular-Demo/src/app/app.component.ts
+++ Angular-Demo/src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular Demo';
-  name = 'Importing name from component.ts';
+  name = '';
 }
--- Angular-Demo/src/app/app.module.ts
+++ Angular-Demo/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {ServerComponent} from './server/server.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
