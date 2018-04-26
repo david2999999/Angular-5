6ea22fd969caae81716ad74dd9ecac23596a73b5
David Jiang <djiang86@binghamton.edu>
2018-04-26 04:50:31 +0000

    created typescript class for component

--- Angular-Basic-Project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular-Basic-Project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Fry Chicken', 'Deep fried chicken recipe is delicious either hot or cold.',
+      'https://cdn3.tmbi.com/secure/RMS/attachments/37/300x300/Crispy-Fried-Chicken_exps6445_PSG143429D03_05_5b_RMS.jpg')
+  ];
 
   constructor() { }
 
