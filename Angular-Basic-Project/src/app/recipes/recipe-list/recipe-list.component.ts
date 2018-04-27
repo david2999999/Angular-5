3e13ec0a39e5c1dc20de3f29ee6b56161d6802d3
David Jiang <djiang86@binghamton.edu>
2018-04-27 02:42:33 +0000

    list of recipe using *ngFor and also recipe detail

--- Angular-Basic-Project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular-Basic-Project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,8 @@ import {Recipe} from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('Fry Chicken', 'Deep fried chicken recipe is delicious either hot or cold.',
-      'https://cdn3.tmbi.com/secure/RMS/attachments/37/300x300/Crispy-Fried-Chicken_exps6445_PSG143429D03_05_5b_RMS.jpg')
+      'https://cdn3.tmbi.com/secure/RMS/attachments/37/300x300/Crispy-Fried-Chicken_exps6445_PSG143429D03_05_5b_RMS.jpg'),
+    new Recipe('Strawberry Cobbler I', 'Serve with cream or ice cream.', 'https://images.media-allrecipes.com/images/55969.jpg')
   ];
 
   constructor() { }
