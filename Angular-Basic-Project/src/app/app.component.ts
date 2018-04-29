62da8020cfac3591fd575269b7d699f0730e1581
David Jiang <djiang86@binghamton.edu>
2018-04-29 14:05:47 +0000

    passing recipe data from the recipe list to recipe-item using property binding

--- Angular-Basic-Project/src/app/app.component.ts
+++ Angular-Basic-Project/src/app/app.component.ts
@@ -6,39 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [{type: 'server', name: 'TestServer', content: 'Just a test!'}];
-  oddNumbers: number[] = [];
-  evenNumbers: number[] = [];
+  loadedFeature = 'recipe';
 
-  onServerAdded(serverData: {serverName: string, serverContent: string}) {
-    this.serverElements.push({
-      type: 'server',
-      name: serverData.serverName,
-      content: serverData.serverContent
-    });
-  }
-
-  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: blueprintData.serverName,
-      content: blueprintData.serverContent
-    });
-  }
-
-  onChangeFirst() {
-    this.serverElements[0].name = 'CHANGEDDDD';
-  }
-
-  onDestroyFirst() {
-    this.serverElements.splice(0, 1);
-  }
-
-  onIntervalFired(firedNumber: number) {
-    if (firedNumber % 2 === 0) {
-      this.evenNumbers.push(firedNumber);
-    }else {
-      this.oddNumbers.push(firedNumber);
-    }
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
   }
 }
