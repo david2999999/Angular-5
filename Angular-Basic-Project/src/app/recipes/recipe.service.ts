1b983fa957b8b078db4aa108aee724a357ea06e8
David Jiang <djiang86@binghamton.edu>
2018-05-07 18:22:12 +0000

    cross component communication with service

--- Angular-Basic-Project/src/app/recipes/recipe.service.ts
+++ Angular-Basic-Project/src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
 import {Recipe} from './recipe.model';
+import {EventEmitter} from '@angular/core';
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe> ();
+
   private recipes: Recipe[] = [
     new Recipe('Fry Chicken', 'Deep fried chicken recipe is delicious either hot or cold.',
       'https://cdn3.tmbi.com/secure/RMS/attachments/37/300x300/Crispy-Fried-Chicken_exps6445_PSG143429D03_05_5b_RMS.jpg'),
