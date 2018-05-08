6663c3ff8322c7e5d9c805033812d68ae947ddb5
David Jiang <djiang86@binghamton.edu>
2018-05-08 17:34:24 +0000

    added shopping list service

--- Angular-Basic-Project/src/app/shopping-list/shopping-list.service.ts
+++ Angular-Basic-Project/src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,24 @@
-export class ShoppingListService {
+import {Ingredient} from '../shared/ingredient.model';
+import {EventEmitter} from '@angular/core';
 
+export class ShoppingListService {
+  ingredientChanged = new EventEmitter<Ingredient[]>();
+
+ private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+    new Ingredient('Chicken Leg', 5),
+    new Ingredient('Salt', 2)
+  ];
+
+ getIngredient() {
+   return this.ingredients.slice();
+ }
+
+ addIngredient(ingredient: Ingredient) {
+   this.ingredients.push(ingredient);
+   this.ingredientChanged.emit(this.ingredients.slice());
+ }
 }
+
+
