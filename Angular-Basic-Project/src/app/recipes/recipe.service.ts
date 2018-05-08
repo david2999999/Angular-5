4bd9413c66071e46fc39055dfa640c08652f944d
David Jiang <djiang86@binghamton.edu>
2018-05-08 17:39:23 +0000

    add ingredient to shopping list

--- Angular-Basic-Project/src/app/recipes/recipe.service.ts
+++ Angular-Basic-Project/src/app/recipes/recipe.service.ts
@@ -1,13 +1,25 @@
 import {Recipe} from './recipe.model';
 import {EventEmitter} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe> ();
 
   private recipes: Recipe[] = [
-    new Recipe('Fry Chicken', 'Deep fried chicken recipe is delicious either hot or cold.',
-      'https://cdn3.tmbi.com/secure/RMS/attachments/37/300x300/Crispy-Fried-Chicken_exps6445_PSG143429D03_05_5b_RMS.jpg'),
-    new Recipe('Strawberry Cobbler I', 'Serve with cream or ice cream.', 'https://images.media-allrecipes.com/images/55969.jpg')
+    new Recipe('Fry Chicken',
+      'Deep fried chicken recipe is delicious either hot or cold.',
+      'https://cdn3.tmbi.com/secure/RMS/attachments/37/300x300/Crispy-Fried-Chicken_exps6445_PSG143429D03_05_5b_RMS.jpg',
+      [
+        new Ingredient('Chicken', 2),
+        new Ingredient('Flour', 2)
+      ]),
+    new Recipe('Strawberry Cobbler I',
+      'Serve with cream or ice cream.',
+      'https://images.media-allrecipes.com/images/55969.jpg',
+      [
+        new Ingredient('Sugar', 10),
+        new Ingredient('Strawberry', 2)
+      ])
   ];
 
   getRecipes() {
