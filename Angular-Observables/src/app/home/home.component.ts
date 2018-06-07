3725d80d8181660784b664554c136dd5b54cf1b0
David Jiang <djiang86@binghamton.edu>
2018-06-07 23:12:14 +0000

    creating custom observers and observables

--- Angular-Observables/src/app/home/home.component.ts
+++ Angular-Observables/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/interval';
+import {Observer} from 'rxjs/Observer';
 
 @Component({
   selector: 'app-home',
@@ -12,14 +13,42 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    // emit value in sequence every 1 second
-    const myNumbers = Observable.interval(1000);
+    // // emit value in sequence every 1 second
+    // const myNumbers = Observable.interval(1000);
+    //
+    // // output: 0,1,2,3,4,5....
+    // myNumbers.subscribe(
+    //   (number: number) => {
+    //     console.log(number);
+    //   }
+    // );
 
-    // output: 0,1,2,3,4,5....
-    myNumbers.subscribe(
-      (number: number) => {
-        console.log(number);
-      }
+    const myObservable = Observable.create((observer: Observer<string>) => {
+      setTimeout(() => {
+        observer.next('first package');
+      }, 2000);
+
+      setTimeout(() => {
+        observer.next('second package');
+      }, 4000);
+
+      // setTimeout(() => {
+      //   observer.error('this does not work');
+      // }, 5000);
+
+      setTimeout(() => {
+        observer.complete();
+      }, 5000);
+
+      setTimeout(() => {
+        observer.next('third');
+      }, 6000);
+    });
+
+    myObservable.subscribe(
+      (data: string) => { console.log(data); },
+      (error: string) => { console.log(error); },
+      () => { console.log('completed'); }
     );
   }
 
