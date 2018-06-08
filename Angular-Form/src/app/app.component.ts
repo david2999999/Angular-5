d8641366907dea67233dcf78bf904f0bb876afcf
David Jiang <djiang86@binghamton.edu>
2018-06-08 23:26:40 +0000

    accessing the form that is submitted

--- Angular-Form/src/app/app.component.ts
+++ Angular-Form/src/app/app.component.ts
@@ -11,6 +11,15 @@ export class AppComponent {
   defaultQuestion = 'pet';
   answer = '';
   genders = ['male', 'female'];
+  submitted = false;
+
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
 
   suggestUsername() {
     const suggestedName = 'Superuser';
@@ -26,7 +35,7 @@ export class AppComponent {
 
     this.signupForm.form.patchValue({
       userData: {
-        username: suggestedName;
+        username: suggestedName
       }
     });
   }
@@ -36,7 +45,12 @@ export class AppComponent {
   // }
 
   onSubmit() {
-    console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
   }
 }
 
