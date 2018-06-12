ac414f9b804583b8b78b6af76b7288c6818d81ae
David Jiang <djiang86@binghamton.edu>
2018-06-12 03:29:49 +0000

    saving data into firebase with put request

--- Angular-Basic-Project/src/app/header/header.component.ts
+++ Angular-Basic-Project/src/app/header/header.component.ts
@@ -1,9 +1,22 @@
 import {Component} from '@angular/core';
+import {DataStorageService} from '../shared/data-storage.service';
+import {Response} from '@angular/http';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: []
 })
-export class HeaderComponent{
+export class HeaderComponent {
+
+  constructor(private dataStorageServer: DataStorageService) {}
+
+  onSaveData() {
+    this.dataStorageServer.storeRecipes()
+      .subscribe(
+        (response: Response) => {
+          console.log(response);
+        }
+      );
+  }
 }
