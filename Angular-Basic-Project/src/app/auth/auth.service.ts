47a9cba1f72c4dc3545cd39dc27815485a097b48
David Jiang <djiang86@binghamton.edu>
2018-06-13 00:28:25 +0000

    sending token along with requests

--- Angular-Basic-Project/src/app/auth/auth.service.ts
+++ Angular-Basic-Project/src/app/auth/auth.service.ts
@@ -1,6 +1,9 @@
 import * as firebase from 'firebase';
+import 'firebase/auth';
 
-export class AuthService{
+export class AuthService {
+  token: string;
+
   signupUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
       .catch(
@@ -12,10 +15,24 @@ export class AuthService{
     // the .then clause will be executed if the user successfully logs in
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(
-        response => console.log(response)
+        response => {
+          firebase.auth().currentUser.getIdToken()
+            .then(
+              (token: string) => this.token = token
+            );
+        }
       )
       .catch(
         error => console.log(error)
       );
+  }
+
+  getToken() {
+    firebase.auth().currentUser.getIdToken()
+      .then(
+        (token: string) => this.token = token
+      );
+
+    return this.token;
   }
 }
--- Angular-Basic-Project/src/app/shared/data-storage.service.ts
+++ Angular-Basic-Project/src/app/shared/data-storage.service.ts
@@ -3,22 +3,27 @@ import {Http, Response} from '@angular/http';
 import {RecipeService} from '../recipes/recipe.service';
 import {Recipe} from '../recipes/recipe.model';
 import 'rxjs/add/operator/map';
+import {AuthService} from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
 
-  constructor(private http: Http, private recipeService: RecipeService) { }
+  constructor(private http: Http, private recipeService: RecipeService, private authService: AuthService) { }
 
   storeRecipes() {
-    return this.http.put('https://angular-udemy-course.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+    const token = this.authService.getToken();
+
+    return this.http.put('https://angular-udemy-course.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
   }
 
   getRecipes() {
+    const token = this.authService.getToken();
+
     // when we delete all ingredients and save the recipe to the firebase
     // the ingredient field will be empty
     // this will cause errors when try to add ingredients again when the recipe
     // is retrieved again, so we used map to transform the data to get new array for recipe[ingredient]
-    return this.http.get('https://angular-udemy-course.firebaseio.com/recipes.json')
+    return this.http.get('https://angular-udemy-course.firebaseio.com/recipes.json?auth=' + token)
       .map(
         (response: Response) => {
           const recipes: Recipe[] = response.json();
