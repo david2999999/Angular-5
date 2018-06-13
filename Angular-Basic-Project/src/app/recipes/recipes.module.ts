505e3eb266a72bfa8026dee13ee5741f1267ec4e
David Jiang <djiang86@binghamton.edu>
2018-06-13 02:20:16 +0000

    created a separate module for recipe

---
+++ Angular-Basic-Project/src/app/recipes/recipes.module.ts
@@ -0,0 +1,29 @@
+import {NgModule} from '@angular/core';
+import {RecipesComponent} from './recipes.component';
+import {RecipeStartComponent} from './recipe-start/recipe-start.component';
+import {RecipeListComponent} from './recipe-list/recipe-list.component';
+import {RecipeEditComponent} from './recipe-edit/recipe-edit.component';
+import {RecipeDetailComponent} from './recipe-detail/recipe-detail.component';
+import {RecipeItemComponent} from './recipe-list/recipe-item/recipe-item.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
+
+@NgModule({
+  declarations: [
+    RecipesComponent,
+    RecipeStartComponent,
+    RecipeListComponent,
+    RecipeEditComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent
+  ],
+  imports: [
+    // the commonModule gives access to common directives like ngClass, ngFor etc
+    // chances that every module will be using these
+    CommonModule,
+    ReactiveFormsModule
+  ]
+})
+export class RecipesModule {
+
+}
