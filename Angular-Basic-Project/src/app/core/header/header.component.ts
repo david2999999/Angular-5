131e37dae6b44b9eeff185d104265baffc058124
David Jiang <djiang86@binghamton.edu>
2018-06-14 02:27:47 +0000

    added core module

---
+++ Angular-Basic-Project/src/app/core/header/header.component.ts
@@ -0,0 +1,31 @@
+import {Component} from '@angular/core';
+import {DataStorageService} from '../../shared/data-storage.service';
+import {Response} from '@angular/http';
+import {AuthService} from '../../auth/auth.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: []
+})
+export class HeaderComponent {
+
+  constructor(private dataStorageServer: DataStorageService, private authService: AuthService) {}
+
+  onSaveData() {
+    this.dataStorageServer.storeRecipes()
+      .subscribe(
+        (response: Response) => {
+          console.log(response);
+        }
+      );
+  }
+
+  onFetchData() {
+    this.dataStorageServer.getRecipes();
+  }
+
+  onLogout() {
+    this.authService.logout();
+  }
+}
