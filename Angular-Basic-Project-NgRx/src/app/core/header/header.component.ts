0152ad219554e060d60970b4e0b537aab96600d4
David Jiang <djiang86@binghamton.edu>
2018-06-16 02:27:48 +0000

    using authentication inside the header component

--- Angular-Basic-Project-NgRx/src/app/core/header/header.component.ts
+++ Angular-Basic-Project-NgRx/src/app/core/header/header.component.ts
@@ -1,17 +1,26 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {DataStorageService} from '../../shared/data-storage.service';
-import {Response} from '@angular/http';
 import {AuthService} from '../../auth/auth.service';
-import {HttpEvent, HttpEventType} from '@angular/common/http';
+import {Store} from '@ngrx/store';
+import * as fromApp from '../../store/app.reducers';
+import {Observable} from 'rxjs/Observable';
+import * as fromAuth from '../../auth/ngrx-store/auth.reducers';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: []
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  authState: Observable<fromAuth.State>;
 
-  constructor(private dataStorageServer: DataStorageService, public authService: AuthService) {}
+  constructor(private dataStorageServer: DataStorageService,
+              public authService: AuthService,
+              private store: Store<fromApp.AppState>) {}
+
+  ngOnInit(): void {
+    this.authState = this.store.select('auth');
+  }
 
   onSaveData() {
     this.dataStorageServer.storeRecipes()
@@ -33,4 +42,5 @@ export class HeaderComponent {
   onLogout() {
     this.authService.logout();
   }
+
 }
