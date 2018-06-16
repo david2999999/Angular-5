85428e626adffe83eb59ce2f8b5a451570cdb14b
David Jiang <djiang86@binghamton.edu>
2018-06-16 02:13:45 +0000

    using ActionReducerMap to create reducers

--- Angular-Basic-Project-NgRx/src/app/app.module.ts
+++ Angular-Basic-Project-NgRx/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {SharedModule} from './shared/shared.module';
@@ -9,7 +8,7 @@ import {AuthModule} from './auth/auth.module';
 import {CoreModule} from './core/core.module';
 import {HttpClientModule} from '@angular/common/http';
 import {StoreModule} from '@ngrx/store';
-import {shoppingListReducer} from './shopping-list/ngrx-store/shopping-list.reducers';
+import {reducers} from './store/app.reducers';
 
 
 @NgModule({
@@ -24,7 +23,7 @@ import {shoppingListReducer} from './shopping-list/ngrx-store/shopping-list.redu
     ShoppingListModule,
     AuthModule,
     CoreModule,
-    StoreModule.forRoot({shoppingList: shoppingListReducer})
+    StoreModule.forRoot(reducers)
   ],
   bootstrap: [AppComponent]
 })
