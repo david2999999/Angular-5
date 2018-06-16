95b9729debdb9286cd61324664465d6950c900a8
David Jiang <djiang86@binghamton.edu>
2018-06-16 02:35:43 +0000

    dispatch actions in the auth service

--- Angular-Basic-Project-NgRx/src/app/auth/auth.service.ts
+++ Angular-Basic-Project-NgRx/src/app/auth/auth.service.ts
@@ -3,14 +3,21 @@ import 'firebase/auth';
 import {Router} from '@angular/router';
 import {Injectable} from '@angular/core';
 
+import {Store} from '@ngrx/store';
+import * as fromApp from '../store/app.reducers';
+import * as AuthActions from './ngrx-store/auth.actions';
+
 @Injectable()
 export class AuthService {
-  token: string;
-
-  constructor(private router: Router) {}
+  constructor(private router: Router, private store: Store<fromApp.AppState>) {}
 
   signupUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then(
+        user => {
+          this.store.dispatch(new AuthActions.Signup());
+        }
+      )
       .catch(
         error => console.log(error)
       );
@@ -21,10 +28,13 @@ export class AuthService {
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(
         response => {
+          this.store.dispatch(new AuthActions.Signin());
           this.router.navigate(['/']);
           firebase.auth().currentUser.getIdToken()
             .then(
-              (token: string) => this.token = token
+              (token: string) => {
+                this.store.dispatch(new AuthActions.SetToken(token));
+              }
             );
         }
       )
@@ -33,21 +43,8 @@ export class AuthService {
       );
   }
 
-  getToken() {
-    firebase.auth().currentUser.getIdToken()
-      .then(
-        (token: string) => this.token = token
-      );
-
-    return this.token;
-  }
-
-  isAuthenticated() {
-    return this.token != null;
-  }
-
   logout() {
     firebase.auth().signOut();
-    this.token = null;
+    this.store.dispatch(new AuthActions.Logout());
   }
 }
