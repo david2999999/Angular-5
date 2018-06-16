cb419066b92f2731ae1318506c928e553f2efacc
David Jiang <djiang86@binghamton.edu>
2018-06-16 01:53:35 +0000

    added reducer for auth

--- Angular-Basic-Project-NgRx/src/app/auth/ngrx-store/auth.reducers.ts
+++ Angular-Basic-Project-NgRx/src/app/auth/ngrx-store/auth.reducers.ts
@@ -1,3 +1,5 @@
+import * as AuthActions from './auth.actions';
+
 export interface  State {
   token: string;
   authenticated: boolean;
@@ -10,6 +12,21 @@ const initialState = {
   authenticated: false
 };
 
-export function authReducer(state = initialState, action) {
-  return state;
+export function authReducer(state = initialState, action: AuthActions.AuthActions) {
+  switch (action.type) {
+    case (AuthActions.SIGNUP):
+    case (AuthActions.SIGNIN):
+      return {
+        ...state,
+        authenticated: true
+      };
+    case (AuthActions.LOGOUT):
+      return {
+        ...state,
+        token: null,
+        authenticated: false
+      };
+    default:
+      return state;
+  }
 }
