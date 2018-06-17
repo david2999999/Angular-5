e8efc3fe048037b06d2d175ef47ae507ab23737c
David Jiang <djiang86@binghamton.edu>
2018-06-17 01:56:06 +0000

    handling state in http interceptor and also retrieving the token

--- Angular-Basic-Project-NgRx/src/app/auth/auth-guard.service.ts
+++ Angular-Basic-Project-NgRx/src/app/auth/auth-guard.service.ts
@@ -1,15 +1,20 @@
 import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot} from '@angular/router';
 import {Observable} from 'rxjs/Observable';
 import {Injectable} from '@angular/core';
-import {AuthService} from './auth.service';
+import {Store} from '@ngrx/store';
+
+import * as fromApp from '../store/app.reducers';
+import * as fromAuth from './ngrx-store/auth.reducers';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-  constructor(private authService: AuthService) {}
+  constructor(private store: Store<fromApp.AppState>) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return this.authService.isAuthenticated();
+    return this.store.select('auth').map((authState: fromAuth.State) => {
+      return authState.authenticated;
+    });
   }
 
 }
--- Angular-Basic-Project-NgRx/src/app/shared/auth.interceptor.ts
+++ Angular-Basic-Project-NgRx/src/app/shared/auth.interceptor.ts
@@ -1,20 +1,27 @@
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
 import {Injectable} from '@angular/core';
-import {AuthService} from '../auth/auth.service';
+import 'rxjs/add/operator/switchMap';
+
+import {Store} from '@ngrx/store';
+import * as fromApp from '../store/app.reducers';
+import * as fromAuth from '../auth/ngrx-store/auth.reducers';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private authService: AuthService) {}
+  constructor(private store: Store<fromApp.AppState>) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     console.log('Intercepted!', req);
-
     // const copiedReq = req.clone({headers: req.headers.append('','')});
-    const copiedReq = req.clone({params: req.params.set('auth', this.authService.getToken())});
 
-    return next.handle(copiedReq);
+    return this.store.select('auth').switchMap(
+      (authState: fromAuth.State) => {
+        const copiedReq = req.clone({params: req.params.set('auth', authState.token)});
+        return next.handle(copiedReq);
+      }
+    );
 
     // this will stop any request
     // return null;
