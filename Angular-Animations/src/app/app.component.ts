10f228d0f6fd0e775f538c7fa597958b47d1a3b5
David Jiang <djiang86@binghamton.edu>
2018-06-30 16:34:11 +0000

    advanced transition with wildcard

--- Angular-Animations/src/app/app.component.ts
+++ Angular-Animations/src/app/app.component.ts
@@ -16,15 +16,39 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
       })),
       transition('normal => highlighted', animate(300)),
       transition('highlighted => normal', animate(800))
+      // transition('highlighted <=> normal', animate(800))
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        'background-color': 'orange',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style({
+        'background-color': 'red',
+        transform: 'translateX(100px) scale(1)'
+      })),
+      state('shrunken', style({
+        'background-color': 'green',
+        transform: 'translateX(0) scale(0.5)'
+      })),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', animate(500))
     ])
   ]
 })
 export class AppComponent {
   state = 'normal';
+  wildState = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
   onAnimate() {
     this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+    this.wildState === 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+  }
+
+  onShrink() {
+    this.wildState = 'shrunken';
   }
 
   onAdd(item) {
