81c10d8d7b0f0e1a13b71d206102b6b900d5cab7
David Jiang <djiang86@binghamton.edu>
2018-06-30 19:39:57 +0000

    keyframe transitions

--- Angular-Animations/src/app/app.component.ts
+++ Angular-Animations/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {animate, state, style, transition, trigger} from '@angular/animations';
+import {animate, keyframes, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -18,7 +18,6 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
       transition('highlighted => normal', animate(800))
       // transition('highlighted <=> normal', animate(800))
     ]),
-
     trigger('wildState', [
       state('normal', style({
         'background-color': 'orange',
@@ -63,6 +62,43 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
         }))
       ])
     ]),
+    trigger('list2', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        animate(1000, keyframes([
+          style({
+            transform: 'translateX(-100px)',
+            opacity: 0,
+            offset: 0
+          }),
+          style({
+            transform: 'translateX(-50px)',
+            opacity: 0.5,
+            offset: 0.3
+          }),
+          style({
+            transform: 'translateX(-20px)',
+            opacity: 1,
+            offset: 0.8
+          }),
+          style({
+            transform: 'translateX(0px)',
+            opacity: 1,
+            offset: 1
+          })
+        ]))
+      ]),
+      transition('* => void', [
+        animate(300, style({
+          transform: 'translateX(100px)',
+          opacity: 0
+        }))
+      ])
+    ]),
+
   ]
 })
 export class AppComponent {
